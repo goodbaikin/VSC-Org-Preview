216c360bcc09b3f9e936ddfc9072df76060bbb5e
goodbaikin <goodbaikin@gmail.com>
2021-02-10 22:17:28 +0000

    implemented pdf-base rendering

--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,75 @@
 import * as vscode from 'vscode';
 import * as util from 'util';
 import * as child_process from 'child_process';
+import * as path from 'path';
+import { PdfPreview } from './previewer';
+
 
 const exec = util.promisify(child_process.exec);
-
-let panel: vscode.WebviewPanel;
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('org-preview.start', () => {
-			panel = vscode.window.createWebviewPanel(
-				'Org Preview',
-				'Org Preview',
+			const panel = vscode.window.createWebviewPanel(
+				'org-preview',
+				"Org Preview",
 				vscode.ViewColumn.Beside,
-				{}
+				{
+					enableFindWidget: false,
+					retainContextWhenHidden: true
+				}
 			);
-			updateWebView();
+			let filePath = vscode.window.activeTextEditor!!.document.uri.fsPath;
+			let dirname = path.dirname(filePath);
+			let name = path.parse(filePath).name;
+			let pdfPath = path.join(dirname, name + ".pdf");
+			let pdfUri = vscode.Uri.file(pdfPath);
+
+			exportOrgToPdf();
+			const preview = new PdfPreview(
+				context.extensionUri,
+				pdfUri,
+				panel
+			);
 		})
 	);
 
 	context.subscriptions.push(
 		vscode.workspace.onDidSaveTextDocument(e => {
-			updateWebView();
+			exportOrgToPdf();
 		})
 	);
 }
 
-async function updateWebView() {
+
+async function exportOrgToPdf() {
 	let editor = vscode.window.activeTextEditor;
 	if (!editor) { return; }
 	editor!!;
 
-	let path = editor.document.fileName;
-	let index = path.lastIndexOf('/');
-	let fileName = path.slice(index + 1, undefined);
-	let dirPath = path.slice(undefined, index);
-	if (isOrgFile(fileName)) {
-		let stderr = await getConvertPromise(dirPath, fileName);
-		console.log(stderr);
+	let pathStr = editor.document.fileName;
+	let extName = path.extname(pathStr);
+	let dirPath = path.dirname(pathStr);
+	let fileName = path.basename(pathStr);
+	if (isOrgFile(extName)) {
+		let result = await getExportPromise(dirPath, fileName);
+		console.log(result);
 	}
-
 }
 
 
-function isOrgFile(fileName: string) {
-	let index = fileName.lastIndexOf('.') + 1;
-	let ext = fileName.slice(index);
-	return ext.toLocaleLowerCase() === "org";
+function isOrgFile(ext: string) {
+	return ext.toLowerCase() === ".org";
 }
 
-function getConvertPromise(dirPath: string, fileName: string) {
+function getExportPromise(dirPath: string, fileName: string) {
 	let cmd = `docker run --rm -v "${dirPath}":/tmp -w /tmp goodbaikin/org2pdf org2pdf "${fileName}"`;
 	let promise = exec(cmd).then(({ stdout, stderr }) => {
-		return stderr;
+		return stdout + "\n" + stderr;
 	});
 	return promise;
 }
+
+
+
+export function deactivate() { }
